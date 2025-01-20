558c85c5201f69cc419941bca20976b434f92843
AidinYazdi <aidinyazdi@gmail.com>
2025-01-19 19:31:52 -0600

    more

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const { readFile } = require('fs').promises;
+
+app.get('/', async (req, res) => {
+    res.send(await readFile('./home.html', 'utf8'));
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Server is listening on port', process.env.PORT);
+});
